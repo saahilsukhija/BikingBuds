c285911b53bde2490b2b1e09114aee2f13fd6763
Saahil Sukhija <saahilsukhija@gmail.com>
2024-06-15 11:54:35 -0700

    Changed map icons

---
+++ CongressionalAppBiking/View Controllers/Initial Launch/Slide.swift
@@ -0,0 +1,22 @@
+//
+//  WhatsNewView.swift
+//  CongressionalAppBiking
+//
+//  Created by Saahil Sukhija on 6/1/24.
+//
+
+import UIKit
+
+class WhatsNewView: UIView {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
